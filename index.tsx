2b845b9efba9cee8dc260388cdbf69869566bab5
5weeeeet <133291016+5weeeeet@users.noreply.github.com>
2025-01-17 15:11:38 +0300

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import VideoChatApp from './video-chat-app';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <VideoChatApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
