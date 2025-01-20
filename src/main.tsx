d6c53446c6fd0887f683497f94cf3ff703333627
5weeeeet <133291016+5weeeeet@users.noreply.github.com>
2025-01-20 08:04:52 +0000

    6

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import ReactDOM from 'react-dom'
+import VideoChatApp from './video-chat-app'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <VideoChatApp />
+  </React.StrictMode>,
+  document.getElementById('root')
 )
