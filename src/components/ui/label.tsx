cddcc5baa15c8bdc6f6f49be298afbbe1353d4dd
5weeeeet <133291016+5weeeeet@users.noreply.github.com>
2025-01-20 13:13:40 +0000

    1

--- src/components/ui/label.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Label = ({ children, ...props }: { children: React.ReactNode, [key: string]: any }) => (
-  <label className="block text-gray-700 mb-2" {...props}>
-    {children}
-  </label>
-)
--- src/components/ui/textarea.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const Textarea = ({ ...props }: { [key: string]: any }) => (
-  <textarea className="border border-gray-300 rounded px-4 py-2 w-full" {...props} />
-)
